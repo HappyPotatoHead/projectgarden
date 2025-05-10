b91e786f4d60913fddcffb266252e2d81e34bbf1
HappyPotatoHead <jimmyding9719@gmail.com>
2025-05-10 14:15:46 +0800

    Quartz sync: May 10, 2025, 2:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "happypotatohead.github.io ",
+    baseUrl: "happypotatohead.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
